d8a15e16528e540d9b8fbddaed8926a423d3ba24
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-06-08 23:31:26 -0400

    fix validator data

--- src/core/middlewares/validatorData.js
+++ src/core/middlewares/validatorData.js
@@ -1,22 +1,11 @@
-import tables from "../database/tableStructures.js";
-import { adapterToDB } from "../actions/adapter.js";
-
 const regular_expression = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
 const validatorData = (req, res, next) => {
-	const direc = req.originalUrl.split("/")[1];
-	const dataTable = tables[direc];
+	const data = req.body;
 
-	const dbProductData = adapterToDB(dataTable, req.body);
-
-	for (const key in dataTable) {
-		const attribute = dataTable[key][0];
-		const type = dataTable[key][1];
-
-		const value = dbProductData[attribute];
-
-		if (regular_expression.test(value) || typeof value !== type) {
-			return res.status(400).send("Invalid data");
+	for (const key in data) {
+		if (regular_expression.test(data[key])) {
+			res.status(400);
 		}
 	}
 
