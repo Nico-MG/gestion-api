6f679bec824d09ab2bafd1a392176751ca961c72
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-06-21 18:12:43 -0400

    add condition coderepeat

--- src/purchases/purchases.service.js
+++ src/purchases/purchases.service.js
@@ -83,7 +83,7 @@ export const updatePurchaseService = async (req) => {
 	if (!purchase) {
 		throw new NotFound("Compra");
 	}
-	if (purchaseCode.length > 1) {
+	if (purchaseCode.length > 1 && purchaseCode[0] !== req.body.cod) {
 		throw new CodeRepeat("compra", req.body.cod);
 	}
 
