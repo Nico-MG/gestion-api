8e5ee4a1bb06049425493606f48036dc14e92952
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-06-21 17:09:38 -0400

    fix priceAjuster product_id

--- src/core/actions/priceAjuster.js
+++ src/core/actions/priceAjuster.js
@@ -7,7 +7,7 @@ export default async function priceAjuster(detalles) {
 	const productos = await getAllProductsService();
 	for (let idx = 0; idx < detalles.length; idx++) {
 		const detalle = detalles[idx];
-		const producto = productos.find((p) => p.idp === detalle.idp);
+		const producto = productos.find((p) => p.idp === detalle.product_id);
 		const catDetalle = detalle.quantity;
 		const catProducto = producto.cit;
 		const priceDetalle = detalle.price;
