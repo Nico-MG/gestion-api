19de7616d7dda44bd4a2894fa05ee0e05d3a3b18
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-07-01 23:12:50 -0400

    added rut, name and lastname to token

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -13,57 +13,54 @@ class HttpError extends Error {
   }
 }
 
-
 const getLoginUser = async (req, res) => {
-	const SECRET_KEY = process.env.SECRET_KEY;
-	const { rutu, pwd } = req.body;
-      
+  const SECRET_KEY = process.env.SECRET_KEY;
+  const { rutu, pwd } = req.body;
 
-	try {
+  try {
+    if (!rutu || !pwd) {
+      throw new HttpError(401, "Campos vacíos");
+    }
 
-              if(!rutu || !pwd){
-		  throw new HttpError(401,"Campos vacíos");
-                }
+    if (!moduleRut(rutu)) {
+      throw new InvalidRut(rutu);
+    }
 
+    const result = await getUser(rutu);
 
-	    
-               if(!moduleRut(rutu)){
-         	     throw new InvalidRut(rutu);
- 
-                }
+    if (!result) {
+      throw new HttpError(401, "Usuario no existe");
+    }
 
-	    
-               const result = await getUser(rutu);
-	        
-                if(!result){
-		    throw new HttpError(401,"Usuario no existe");
-       	       }
+		const rut = result.rut;
+    const role = result.role;
+		const name = result.name;
+		const lastname = result.lastname;
 
-	    
-	        const role = result.role;
+    if (!result || !bcrypt.compareSync(pwd, result.password)) {
+      throw new HttpError(401, "Credenciales incorrectas");
+    }
 
-		if (!result || !bcrypt.compareSync(pwd, result.password)) {
-		    throw new HttpError(401,"Credenciales incorrectas");
+    const token = jwt.sign({ rut, role, name, lastname }, SECRET_KEY, { expiresIn: "1h" });
+    const serialized = cookie.serialize("my-token", token, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 1000 * 60 * 60,
+      path: "/",
+      partition: "By-Site",
+    });
 
-		}
-
-		const token = jwt.sign({ role }, SECRET_KEY, { expiresIn: "1h" });
-                const serialized = cookie.serialize("my-token", token, {
-			httpOnly: true,
-      		        sameSite: 'None',
-		        secure: true,
-			maxAge: 1000 * 60 * 60,
-		        path: '/',
-		        partition : 'By-Site'
-		});
-
-		//res.setHeader("Set-Cookie", serialized);
-	       return { status: 200, message:  "Bienvenido!", credentials : token };
-
-	} catch (HttpError) {
-            res.setHeader("Set-Cookie", "NULL");
-	    return { status: HttpError.statusCode ? HttpError.statusCode : 500, message: HttpError.message , credentials : ""};
-	}
+    //res.setHeader("Set-Cookie", serialized);
+    return { status: 200, message: "Bienvenido!", credentials: token };
+  } catch (HttpError) {
+    res.setHeader("Set-Cookie", "NULL");
+    return {
+      status: HttpError.statusCode ? HttpError.statusCode : 500,
+      message: HttpError.message,
+      credentials: "",
+    };
+  }
 };
 
 export default getLoginUser;
