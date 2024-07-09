d11b6461b9e3b3fdbe18bc08d22dc4cfa6ed47b9
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-07-08 22:46:23 -0400

    changed filterHelper for numbers filter

--- src/core/actions/filterHelper.js
+++ src/core/actions/filterHelper.js
@@ -1,94 +1,90 @@
 const isNumberValor = (field) => {
-	return (
-		field === "price" ||
-		field === "quantity" ||
-		field === "min_quantity" ||
-		field === "price" ||
-		field === "line_total" ||
-		field === "unit_price" ||
-		field === "total_price"
-	);
+  return (
+    field === "price" ||
+    field === "quantity" ||
+    field === "min_quantity" ||
+    field === "price" ||
+    field === "line_total" ||
+    field === "unit_price" ||
+    field === "total_price"
+  );
 };
 
 export default function filterHelper(
-	iMap,
-	data,
-	{
-		desde,
-		hasta,
-		dato,
-		valor,
-		limit,
-		offset,
-		orden,
-		mayor,
-		menor,
-		reciente,
-		intervalo,
-	},
+  iMap,
+  data,
+  {
+    desde,
+    hasta,
+    dato,
+    valor,
+    limit,
+    offset,
+    orden,
+    mayor,
+    menor,
+    reciente,
+    intervalo,
+  }
 ) {
-	// Validación de parámetros
-	dato = dato ? iMap[dato] : Object.values(iMap)[0];
-	orden = orden || "";
-	limit = Number.parseInt(limit) || 10;
-	offset = Number.parseInt(offset) || 0;
-	desde = desde || "2000-01-01";
-	hasta = hasta || "2099-12-31";
-	valor = isNumberValor(dato) && valor ? Number.parseInt(valor) : valor || "";
-	mayor = mayor ? Number.parseInt(mayor) : 0;
-	menor = menor ? Number.parseInt(menor) : 1000000;
-	reciente = reciente || "";
-	intervalo = intervalo || "igual";
+  // Validación de parámetros
+  dato = dato ? iMap[dato] : Object.values(iMap)[0];
+  orden = orden || "";
+  limit = Number.parseInt(limit) || 10;
+  offset = Number.parseInt(offset) || 0;
+  desde = desde || "2000-01-01";
+  hasta = hasta || "2099-12-31";
+  valor = valor ? (isNumberValor(dato) ? Number.parseInt(valor) : valor) : "";
+  mayor = mayor ? Number.parseInt(mayor) : 0;
+  menor = menor ? Number.parseInt(menor) : 1000000;
+  reciente = reciente || "";
+  intervalo = intervalo || "igual";
 
-	// Filtro de fecha
-	let result = iMap.fecha
-		? data.filter((item) => item.date >= desde && item.date <= hasta)
-		: data.filter(
-				(item) =>
-					item.createdAt >= new Date(desde) &&
-					item.createdAt <= new Date(hasta),
-			);
+  // Filtro de fecha
+  let result = iMap.fecha
+    ? data.filter((item) => item.date >= desde && item.date <= hasta)
+    : data.filter(
+        (item) =>
+          item.createdAt >= new Date(desde) && item.createdAt <= new Date(hasta)
+      );
 
-	// Ordenar por fecha
-	result =
-		orden === "desc"
-			? result.sort((a, b) => b.createdAt - a.createdAt)
-			: result;
-	result =
-		orden === "asc" ? result.sort((a, b) => a.createdAt - b.createdAt) : result;
+  // Ordenar por fecha
+  result =
+    orden === "desc"
+      ? result.sort((a, b) => b.createdAt - a.createdAt)
+      : result;
+  result =
+    orden === "asc" ? result.sort((a, b) => a.createdAt - b.createdAt) : result;
 
-	// Ordenación
-	result = orden === "desc" ? result.sort((a, b) => b[dato] - a[dato]) : result;
-	result = orden === "asc" ? result.sort((a, b) => a[dato] - b[dato]) : result;
+  // Ordenación
+  result = orden === "desc" ? result.sort((a, b) => b[dato] - a[dato]) : result;
+  result = orden === "asc" ? result.sort((a, b) => a[dato] - b[dato]) : result;
 
 	// Filtro de numero
-	if (isNumberValor(dato) && intervalo === "igual") {
-		result = result.filter((item) => item[dato] === valor);
-	} else if (valor && !isNumberValor(dato)) {
-		result = result.filter((item) =>
-			item[dato].toLowerCase().includes(valor.toLowerCase()),
-		);
-	}
+  if (isNumberValor(dato)) {
+    if (valor !== "") {
+      switch (intervalo) {
+        case "igual":
+          result = result.filter((item) => item[dato] === valor);
+          break;
+        case "mayor":
+          result = result.filter((item) => item[dato] > valor);
+          break;
+        case "menor":
+          result = result.filter((item) => item[dato] < valor);
+          break;
+        default:
+          break;
+      }
+    }
+  } else {
+		// Filtro de texto
+    result = result.filter((item) =>
+      item[dato].toLowerCase().includes(valor.toLowerCase())
+    );
+  }
 
-	// Filtro de rango de valores
-	result = isNumberValor(dato)
-		? result.filter((item) => item[dato] >= mayor)
-		: result;
-	result = isNumberValor(dato)
-		? result.filter((item) => item[dato] <= menor)
-		: result;
-
-	// Filtro de intervalo
-	result =
-		intervalo === "mayor" && isNumberValor(dato)
-			? result.filter((item) => item[dato] > valor)
-			: result;
-	result =
-		intervalo === "menor" && isNumberValor(dato)
-			? result.filter((item) => item[dato] < valor)
-			: result;
-
-	// Paginación
-	result = result.slice(offset, offset + limit);
-	return result;
+  // Paginación
+  result = result.slice(offset, offset + limit);
+  return result;
 }
