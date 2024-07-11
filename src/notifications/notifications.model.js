07b570a1f87aebb8907d769e7ddbfe73db7b340f
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-07-11 03:04:42 -0400

    orderBy notifications by desc

--- src/notifications/notifications.model.js
+++ src/notifications/notifications.model.js
@@ -1,32 +1,34 @@
 import db from "../core/database/connection.js";
 
 export const getAllNotifications = async () => {
-	return await db.notifications.findMany();
+  return await db.notifications.findMany({
+    orderBy: {
+      created_at: "desc",
+    },
+  });
 };
 
-export const getNotification = async (id,idp) => {
-	return db.notifications.findUnique({
-	    where: {
-		notification_id: id,
-	    },
-	    include: {
-		products: true,
-	    }
-	});
+export const getNotification = async (id, idp) => {
+  return db.notifications.findUnique({
+    where: {
+      notification_id: id,
+    },
+    include: {
+      products: true,
+    },
+  });
 };
 
 export const createNotifications = async (data) => {
-	return await db.notifications.create({
-		data,
-	});
+  return await db.notifications.create({
+    data,
+  });
 };
 
 export const deleteNotifications = async (id) => {
-	return await db.notifications.delete({
-		where: {
-			notification_id: id,
-		}
-	});
+  return await db.notifications.delete({
+    where: {
+      notification_id: id,
+    },
+  });
 };
-
-
