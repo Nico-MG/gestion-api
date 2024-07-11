749bb6a2aeb00a56ef98ac5f244bc6cc9fc331e7
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-07-10 21:23:44 -0400

    semi added token validation ws

--- src/server.js
+++ src/server.js
@@ -17,17 +17,17 @@ import salesRoute from "./sales/sales.controller.js";
 import usersRoute from "./users/users.controller.js";
 import authRoute from "./auth/auth.controller.js";
 import analyticsRoute from "./analytics/analytics.controller.js";
-import notificationsRoute from "./notifications/notifications.controller.js"
+import notificationsRoute from "./notifications/notifications.controller.js";
 
 const server = express();
 const port = process.env.PORT || 3000;
 export const wss = new WebSocketServer({ server });
 
 server.use(
-	cors({
-		origin: "http://localhost:1420",
-		credentials: true,
-	}),
+  cors({
+    origin: "http://localhost:1420",
+    credentials: true,
+  })
 );
 server.use(express.json());
 server.disable("x-powered-by");
@@ -35,7 +35,7 @@ server.disable("x-powered-by");
 server.use("/products", validatorData, verifyToken, productsRoute);
 server.use("/users", validatorData, verifyToken, validatorRole, usersRoute);
 server.use("/providers", validatorData, verifyToken, providersRoute);
-server.use("/purchases", validatorData, verifyToken , purchasesRoute);
+server.use("/purchases", validatorData, verifyToken, purchasesRoute);
 server.use("/sales", validatorData, verifyToken, salesRoute);
 server.use("/refunds", validatorData, verifyToken, refundsRoute);
 server.use("/auth", authRoute);
@@ -43,36 +43,50 @@ server.use("/analytics", analyticsRoute);
 server.use("/notifications", validatorData, notificationsRoute);
 
 server.use("/test", validatorData, verifyToken, validatorRole, (req, res) => {
-	wss.clients.forEach((client) => {
-		client.send("Testeado");
-	});
+  wss.clients.forEach((client) => {
+    client.send("Testeado");
+  });
 
-	res.status(200).json({ message: "Hello world!" });
+  res.status(200).json({ message: "Hello world!" });
 });
 
 server
-	.listen(port, () => {
-		console.log(`Server ready to listen on port: ${port}`);
-	})
-	.on("error", (err) => {
-		if (err.code === "EADDRINUSE") {
-			console.error(
-				`Port ${port} is already in use. Please use a different port.`,
-			);
-		} else {
-			console.error(`Server error: ${err}`);
-		}
-	})
-	.on("upgrade", (req, socket, head) => {
-		wss.handleUpgrade(req, socket, head, (ws) => {
-			wss.emit("connection", ws, req);
-		});
-	});
+  .listen(port, () => {
+    console.log(`Server ready to listen on port: ${port}`);
+  })
+  .on("error", (err) => {
+    if (err.code === "EADDRINUSE") {
+      console.error(
+        `Port ${port} is already in use. Please use a different port.`
+      );
+    } else {
+      console.error(`Server error: ${err}`);
+    }
+  })
+  .on("upgrade", (req, socket, head) => {
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit("connection", ws, req);
+    });
+  });
 
 wss.on("connection", (ws, req) => {
-	ws.on("open", () => {
-		console.log("User connected");
-	});
+  ws.on("open", () => {
+    console.log("User connected");
+  });
+  ws.on("message", (message) => {
+    const { token } = JSON.parse(message);
+    try {
+      const payload = jwt.verify(token, process.env.SECRET_KEY);
+      ws.send(JSON.stringify({ status: "valid" }));
+    } catch (error) {
+      ws.send(
+        JSON.stringify({
+          status: "invalid",
+          message: "Sesión expirada o inválida",
+        })
+      );
+    }
+  });
 });
 
-export default  server;
+export default server;
